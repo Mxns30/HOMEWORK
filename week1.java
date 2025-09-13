2fb9e136d6391a1fb73ca9afaca2e90b55e50312
minseo <gangmseo@naver.com>
2025-09-13 12:23:30 +0900

    곱하기 나누기 추가 commit2

--- week1.java
+++ week1.java
@@ -6,5 +6,14 @@ public class week1 {
     
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
-    }
+
+
+        // 두번쨰
+        System.out.println("a * b = " + (a * b));
+         if (b != 0) {
+            System.out.println("a / b = " + ((double)a / b));       
+        } else {
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+    }   
 }
